7a120a5259eb2f18057039b73f543d3bcd29600f
Facundo Abbas <facundoabbas@hotmail.com>
2022-06-19 22:15:59 -0300

    Jsons IMPL

--- Program.cs
+++ Program.cs
@@ -1,20 +1,59 @@
 ﻿using System;
+using System.Text.Json;
+using System.Text;
 namespace Juego{
     class Program
     {
         static void Main(string[] args){
-
+            int cant , flag = 0;
             Peleas match = new Peleas();
             Personaje Winner = new Personaje();
+            Console.WriteLine("Desea volver a jugar con los personajes de la partidas anterior? 1- Si o 2 - No");
+            int opcion14 = Convert.ToInt32(Console.ReadLine());
             List<Personaje> Jugadores  = new List<Personaje>(); 
-            Console.WriteLine("Cuantos jugadores desea crear?");
-            int cant = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < cant; i++)
+            if (opcion14 == 1)
             {
-                Personaje p1 = new Personaje();
-                p1.CrearPersonaje();
-                Jugadores.Add(p1);
+                string path = @"C:\Users\facun\OneDrive\Escritorio\RPG\rpg-2022-Facas27\Personajes.json";
+                if(!File.Exists(path)){
+                    File.Create(path);
+                    File.Create(path).Close();
+                    Console.WriteLine("No se puede cargar partida porque no existe");
+                    flag = 1;
+
+                }else
+                {
+                    string jsonread = File.ReadAllText(path);
+                    Jugadores = JsonSerializer.Deserialize<List<Personaje>>(jsonread);
+
+                }
+                cant = Jugadores.Count();
+                
+            }else
+            {
+                Console.WriteLine("Cuantos jugadores desea crear?");
+               cant = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < cant; i++)
+                {
+                    Personaje p1 = new Personaje();
+                    p1.CrearPersonaje();
+                    Jugadores.Add(p1);
+                }
+                
             }
+            if (flag == 1)
+            {
+                Console.WriteLine("Cuantos jugadores desea crear?");
+               cant = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < cant; i++)
+                {
+                    Personaje p1 = new Personaje();
+                    p1.CrearPersonaje();
+                    Jugadores.Add(p1);
+                }
+                
+            }
+           
+            
             Winner = Jugadores[0];
             for (int i = 0; i < cant; i++)
             {
@@ -40,6 +79,15 @@ namespace Juego{
             if (opc12 == 1)
             {
                 MostrarScore();
+            }
+            Console.WriteLine("Desea guardar los datos de la partida? 1- Si// 2- NO");
+            opcion14 = Convert.ToInt32(Console.ReadLine());
+            if (opcion14 == 1)
+            {
+                string path = @"C:\Users\facun\OneDrive\Escritorio\RPG\rpg-2022-Facas27\Personajes.json";
+                string json = JsonSerializer.Serialize(Jugadores);
+                File.WriteAllText(path, json);
+                
             }
 
 
